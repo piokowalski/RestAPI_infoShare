8fd6988e59a225823d0c54f1261e5cec09110bcb
Michal Nowakowski <michal@nowakowski.me.uk>
2019-05-07 14:01:03 +0200

    Cwiczenie 7 (I)

---
+++ states-client/src/main/java/com/infoshareacademy/api/StatesClientImpl.java
@@ -0,0 +1,17 @@
+package com.infoshareacademy.api;
+
+import com.infoshareacademy.api.model.StateDetails;
+import java.util.List;
+
+public class StatesClientImpl implements StatesClient {
+
+    @Override
+    public List<StateDetails> getAllStates() {
+        return null;
+    }
+
+    @Override
+    public StateDetails getState(String code) {
+        return null;
+    }
+}
