03b1b1119bbd098081955031022aaa1dc9f0344d
piokowalski <piokowalski@onet.pl>
2019-05-07 14:06:27 +0200

    Last exercise - US states tests to pass

---
+++ states-client/src/main/java/com/infoshareacademy/api/StatesClientImpl.java
@@ -0,0 +1,18 @@
+package com.infoshareacademy.api;
+
+import com.infoshareacademy.api.model.StateDetails;
+import com.sun.tools.javac.util.List;
+
+public class StatesClientImpl implements StatesClient {
+
+    @Override
+    public List<StateDetails> getAllStates() {
+        return null;
+    }
+
+    @Override
+    public StateDetails getState(String code) {
+        return null;
+    }
+}
+
--- states-client/src/main/java/com/infoshareacademy/api/model/StateDetails.java
+++ states-client/src/main/java/com/infoshareacademy/api/model/StateDetails.java
@@ -1,6 +1,50 @@
 package com.infoshareacademy.api.model;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StateDetails {
 
-    // fields here (name, code, etc)
+    private String abbr;
+
+    private String name;
+
+    private String largestCity;
+
+    private String capital;
+
+    public StateDetails() {
+    }
+
+    public String getAbbr() {
+        return abbr;
+    }
+
+    public void setAbbr(String abbr) {
+        this.abbr = abbr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLargestCity() {
+        return largestCity;
+    }
+
+    public void setLargestCity(String largestCity) {
+        this.largestCity = largestCity;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
 }
