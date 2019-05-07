a95579e9877afba77de64480fd7feb3c46d91814
Michal Nowakowski <michal@nowakowski.me.uk>
2019-05-07 12:36:53 +0200

    Cwiczenie 6 (II)

--- google-client/src/test/java/com/infoshareacademy/api/GoogleTranslateTest.java
+++ google-client/src/test/java/com/infoshareacademy/api/GoogleTranslateTest.java
@@ -2,15 +2,21 @@ package com.infoshareacademy.api;
  
  import static org.hamcrest.core.Is.is;
  import static org.junit.Assert.assertThat;
- 
+ import static org.junit.Assert.assertTrue;
+
  import org.junit.Before;
+ import org.junit.Rule;
  import org.junit.Test;
- 
- public class GoogleTranslateTest {
+ import org.junit.rules.ExpectedException;
+
+public class GoogleTranslateTest {
  
      public static final String API_KEY = "AIzaSyDU8mB0ggBXlq3j5n_RChUAyfJh68KZTiU";
  
      private GoogleTranslate googleTranslate;
+
+     @Rule
+     public ExpectedException thrown = ExpectedException.none();
  
      @Before
      public void setUp() {
@@ -25,5 +31,19 @@ package com.infoshareacademy.api;
  
          final String result = googleTranslate.translate(input, source, target);
          assertThat(result, is("Cześć jak się masz?"));
+     }
+
+     @Test
+     public void shouldHandleErrors() {
+         thrown.expect(IllegalStateException.class);
+         thrown.expectMessage("API key not valid. Please pass a valid API key.");
+
+         googleTranslate = new GoogleTranslate("dummy key");
+
+         final String input = "Hi, how are you?";
+         final String source = "en";
+         final String target = "pl";
+
+         googleTranslate.translate(input, source, target);
      }
  }
