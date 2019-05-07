2a06ba790b95997eceb44898abcbb74b66d2e5c8
piokowalski <piokowalski@onet.pl>
2019-05-07 11:21:24 +0200

    Exercise 4

--- google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
+++ google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
@@ -1,15 +1,34 @@
 package com.infoshareacademy.api;
- 
- public class GoogleTranslate {
- 
-     private final String API_KEY;
- 
-     public GoogleTranslate(String API_KEY) {
-         this.API_KEY = API_KEY;
-     }
- 
-     public String translate(String input, String source, String target) {
-         throw new UnsupportedOperationException("Not implemented yet");
 
-     }
- }
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class GoogleTranslate {
+
+    private final String API_KEY;
+
+    public GoogleTranslate(String API_KEY) {
+        this.API_KEY = API_KEY;
+    }
+
+    public String translate(String input, String source, String target) {
+
+        String address = "https://translation.googleapis.com/language/translate/v2"
+                + "?key=" + API_KEY
+                + "&q=" + input
+                + "&source=" + source
+                + "&target=" + target;
+
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(address);
+        Response response = webTarget.request().get();
+
+        String result = response.readEntity(String.class);
+
+        response.close(); // !!!!
+
+        return result;
+    }
+}
